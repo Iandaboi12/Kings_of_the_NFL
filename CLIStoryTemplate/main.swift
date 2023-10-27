47b29abd4eb6292ee66e0977581ec3ae0547f33e
Francesca <walzfran@gmail.com>
2023-10-27 10:27:03 -0400

    Added Teams, Scores, Players

--- CLIStoryTemplate/main.swift
+++ CLIStoryTemplate/main.swift
@@ -5,6 +5,106 @@
 
 import Foundation
 
+// Regular Season teams
+let KCC = "Kansas City Chiefs"
+let KCCScore = 57
+let SS = "Seattle Seahawks"
+let SSScore = 51
+let AF = "Atlanta Falcons"
+let AFScore = 41
+let GBP = "Green Bay Packers"
+let GBPScore = 33
+let CP = "Carolina Panthers"
+let CPScore = 10
+let TBB = "Tampa Bay Buccaneers"
+let TBBScore = 40
+let LVR = "Las Vegas Raiders"
+let LVRScore = 25
+let LAC = "Los Angeles Chargers"
+let LACScore = 42
+let CB = "Chicago Bears"
+let CBScore = 26
+let NOS = "New Orleans Saints"
+let NOSScore = 35
+let DB = "Denver Broncos"
+let DBScore = 34
+let MV = "Minnesota Vikings"
+let MVScore = 46
+let DC = "Dallas Cowboys"
+let DCScore = 50
+// Playoff & Superbowl teams
+let NYG = "New York Giants"
+let NYGScore = 40
+let MD = "Miami Dolphins"
+let MDScore = 50
+
+// Lions Team Players
+let QB1 = "Matthew Stafford"
+let QB1Score = 10
+let QB2 = "Bobby Lane"
+let QB2Score = 8
+let QB3 = "Jared Goff"
+let QB3Score = 7
+let QB4 = "Greg Landry"
+let QB4Score = 7
+let QB5 = "Scott Mitchell"
+let QB5Score = 6
+
+let RB1 = "Barry Sanders"
+let RB1Score = 10
+let RB2 = "Billy Sims"
+let RB2Score = 9
+let RB3 = "Doack Walker"
+let RB3Score = 8
+let RB4 = "Mel Farr"
+let RB4Score = 8
+let RB5 = "David Mont"
+let RB5Score = 8
+let RB6 = "D'Andre Swift"
+
+let WR1 = "Calvin Johnson 'Megatron'"
+let WR1Score = 10
+let WR2 = "Herman Moore"
+let WR2Score = 10
+let WR3 = "Golden Tate III"
+let WR3Score = 9
+let WR4 = "Johnnie Morton"
+let WR4Score = 9
+let WR5 = "Amon-Ra St. Brown"
+let WR5Score = 8
+let WR6 = "Marvin Jones"
+let WR6Score = 6
+let WR7 = "Jameson Williams"
+let WR7Score = 5
+
+let D1P1 = "Joe Schmidt"
+let D1P1Score = 10
+let D1P2 = "Lem Barney"
+let D1P2Score = 10
+let D1P3 = "Brian Branch"
+let D1P3Score = 5
+let D1P4 = "Bennie Blades"
+let D1P4Score = 8
+
+let D2P1 = "James Houston"
+let D2P1Score = 6
+let D2P2 = "Aidan Hutchinson"
+let D2P2Score = 7
+let D2P3 = "Dick LeBeau"
+let D2P3Score = 9
+let D2P4 = "Richard Lane"
+let D2P4Score = 9
+
+let D3P1 = "C.J. Gardner-Johnson"
+let D3P1Score = 8
+let D3P2 = "Alex Anzalone"
+let D3P2Score = 7
+let D3P3 = "Jack Campbell"
+let D3P3Score = 6
+let D3P4 = "Jack Christiansen"
+let D3P4Score = 9
+
+
 func readStory() {
     chapterOne()
     chapterTwo()
